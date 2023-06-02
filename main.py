0c37d9d07b647bfcd180048b495bbabbcdd974fa
Hanlin <dhl2011dj@126.com>
2023-06-02 21:27:16 +0100

    finish abaqus demo computing

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import abaqus_interaction
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    verification = multiprocessing.Process(target=abaqus_interaction.verification, args=())
-    verification.start()
-    verification.join()
+    # verification = multiprocessing.Process(target=abaqus_interaction.verification, args=())
+    # verification.start()
+    # verification.join()
 
     abaqus = multiprocessing.Process(target=abaqus_interaction.run_cae, args=())
     abaqus.start()
